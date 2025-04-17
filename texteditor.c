a8e82af7718269de6579e0b6f98684f53c921f9a
Roman Minakov <minakoffroman2015@gmail.com>
2025-04-17 23:06:30 +0200

    movement

--- texteditor.c
+++ texteditor.c
@@ -14,6 +14,7 @@
 
 /*** data ***/
 struct editorConfig{
+	int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -108,11 +109,18 @@ void editorDrawRows(struct abuff* ab){
 
 void editorRefreshScreen(){
 	struct abuff ab = ABUF_INIT;
+
 	abAppend(&ab, "\x1b[?25l", 6);
 	abAppend(&ab, "\x1b[H", 3);
+
 	editorDrawRows(&ab);	
-	abAppend(&ab, "\x1b[H", 3);
+
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+	abAppend(&ab, buf, strlen(buf));
+
 	abAppend(&ab, "\x1b[?25h", 6);
+
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
 }
@@ -124,9 +132,27 @@ char editorReadKey(){
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
 		if (nread == -1 && errno!=EAGAIN) die("read");
 	}
-	return c;
+	if (c=='\x1b'){
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '['){
+			switch (seq[1]){
+				case 'A': return 'w';
+				case 'B': return 's';
+				case 'C': return 'd';
+				case 'D': return 'a';
+			}
+		}
+		return '\x1b';
+	} else{
+		return c;
+	}
 }
 
+void editorMoveCursor(char key);
 void editorProcessKeyPress(){
 	struct abuff ab = ABUF_INIT;
 	char c = editorReadKey();
@@ -138,11 +164,36 @@ void editorProcessKeyPress(){
 			abFree(&ab);
 			exit(0);
 			break;
+		case 'w':
+		case 'a':
+		case 's':
+		case 'd':
+			editorMoveCursor(c);
+			break;
+	}
+}
+
+void editorMoveCursor(char key){
+	switch(key){
+		case 'a':
+			E.cx--;
+			break;
+		case 'd':
+			E.cx++;
+			break;
+		case 'w':
+			E.cy--;
+			break;
+		case 's':
+			E.cy++;
+			break;
 	}
 }
 
 /*** init ***/
 void initEditor(){
+	E.cx = 0;
+	E.cy = 0;
 	if (getWindowSize(&E.screenrows, &E.screencols)==-1) die("getWindowSize");
 }
 
